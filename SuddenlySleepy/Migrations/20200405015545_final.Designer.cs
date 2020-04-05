0827d1d359b0cb0a94c5e865cd87155fa2a4c355
Katie Strauss <straussk@my.lanecc.edu>
2020-04-04 19:22:33 -0700

    deployed

--- SuddenlySleepy/Migrations/20200405015545_final.Designer.cs
+++ SuddenlySleepy/Migrations/20200405015545_final.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SuddenlySleepy.Models;
 
 namespace SuddenlySleepy.Migrations
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200405015545_final")]
+    partial class final
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SuddenlySleepy/Migrations/20200405015545_final.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuddenlySleepy.Migrations
+{
+    public partial class final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SSUserSSEvent_SSEvents_SSEventId",
+                table: "SSUserSSEvent");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SSUserSSEvent_AspNetUsers_SSUserId",
+                table: "SSUserSSEvent");
+
+            migrationBuilder.RenameColumn(
+                name: "SSEventId",
+                table: "SSUserSSEvent",
+                newName: "sSEventId");
+
+            migrationBuilder.RenameColumn(
+                name: "SSUserId",
+                table: "SSUserSSEvent",
+                newName: "sSUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SSUserSSEvent_SSEventId",
+                table: "SSUserSSEvent",
+                newName: "IX_SSUserSSEvent_sSEventId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SSEventName",
+                table: "SSEvents",
+                maxLength: 75,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "SSEvents",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "SSEvents",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SSUserSSEvent_SSEvents_sSEventId",
+                table: "SSUserSSEvent",
+                column: "sSEventId",
+                principalTable: "SSEvents",
+                principalColumn: "SSEventId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SSUserSSEvent_AspNetUsers_sSUserId",
+                table: "SSUserSSEvent",
+                column: "sSUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SSUserSSEvent_SSEvents_sSEventId",
+                table: "SSUserSSEvent");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SSUserSSEvent_AspNetUsers_sSUserId",
+                table: "SSUserSSEvent");
+
+            migrationBuilder.RenameColumn(
+                name: "sSEventId",
+                table: "SSUserSSEvent",
+                newName: "SSEventId");
+
+            migrationBuilder.RenameColumn(
+                name: "sSUserId",
+                table: "SSUserSSEvent",
+                newName: "SSUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SSUserSSEvent_sSEventId",
+                table: "SSUserSSEvent",
+                newName: "IX_SSUserSSEvent_SSEventId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SSEventName",
+                table: "SSEvents",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 75);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "SSEvents",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "SSEvents",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 5000);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SSUserSSEvent_SSEvents_SSEventId",
+                table: "SSUserSSEvent",
+                column: "SSEventId",
+                principalTable: "SSEvents",
+                principalColumn: "SSEventId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SSUserSSEvent_AspNetUsers_SSUserId",
+                table: "SSUserSSEvent",
+                column: "SSUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
